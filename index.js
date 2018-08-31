c3056efd97775a769645073c29f049a901fed30d
Luoana Chirita <luoana.livadariu@gmail.com>
2018-08-31 02:51:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
 const returnFirstTwoDrivers = function (drivers) {
-  const firstTwo = drivers.slice(0, 2)
-  return firstTwo
+  return drivers.slice(0, 2)
+  
 }
 
 const returnLastTwoDrivers = function(drivers) {
